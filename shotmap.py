986e066a576e16e8b935c6b001f8d4603e5e84c0
AidenLyons1 <s2151155@ed.ac.uk>
2024-09-04 10:51:59 +0100

    Up: Adding text

--- shotmap.py
+++ shotmap.py
@@ -84,7 +84,7 @@ class GoalMap:
         ax1.text(
             x=0.5,
             y=0.7,
-            s=f"All shots in the {self.season}-{(int(self.season) + 1) % 100}",
+            s=f"All shots in the {self.season}-{(int(self.season) + 1) % 100} season",
             fontsize=14,
             fontweight="bold",
             fontproperties=font_props,
